f2cefd2f9959c0a03f7b31836cf0b66f8c4670fb
suyikun <270081841@qq.com>
2022-07-26 09:41:56 +0800

    init

---
+++ quickswift/quickswift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  quickswift
+//
+//  Created by suyikun on 2022/7/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
